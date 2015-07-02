42e1ab8c0ee807bffebb78408ddf527cb83b4bbd
Ryan Waples <ryan.waples@gmail.com>
2015-07-02 11:52:32 -0700

    further developed classification methdods, switch from milk to scikit learn

--- R/random_forest.R
+++ R/random_forest.R
@@ -6,16 +6,23 @@ set.seed(71)
 iris.rf <- randomForest(Species ~ ., data=iris, importance=TRUE, proximity=TRUE)
 
 
-
 training_data = read.table('/home/ipseg/Desktop/waples/RAD_allele_depth/data/chinook.training.txt', sep = "\t", header = TRUE)
 unknown_data = read.table('/home/ipseg/Desktop/waples/RAD_allele_depth/data/chinook.unknown.txt', sep = "\t", header = TRUE)
 
 training_data$Duplicate = as.factor(training_data$Duplicate)
 
-loci.rf <- randomForest(factor(Duplicate) ~ depth_per_het + abs_z,
+loci.rf <- randomForest(Duplicate ~ depth_per_het * abs_z,
                         data = training_data, ntree=200, mtry=2,  importance=TRUE, proximity = TRUE)
 
-loci.rf
+names(loci.rf)
+loci.rf$proximity
+
+my_mds <- cmdscale(1-loci.rf$proximity,eig=TRUE, k=3)
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
+     main="Metric  MDS",	type="n")
+
 
 plot(loci.rf)
 plot(margin(loci.rf))
